1952f2c66c5d8e0669ed68a58edf00697f0c9e0b
Rohan Govande <rohanagovande@eaton.com>
2020-06-21 16:19:18 +0530

    Add

---
+++ softmax/src/main/groovy/com/simplilearn/softmax/SoftmaxApplication.groovy
@@ -0,0 +1,13 @@
+package com.simplilearn.softmax
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SoftmaxApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SoftmaxApplication, args)
+	}
+
+}
